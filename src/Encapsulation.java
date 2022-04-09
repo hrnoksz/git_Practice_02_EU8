bafe3f818e968302abb27fe5bb2ec03eec0d18ec
hrnoksz <harun1973@yahoo.com>
2022-04-09 22:39:05 +0300

    set is created

--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -2,4 +2,15 @@ public class Encapsulation {
 
     private String musicInstrument;
 
+    public Encapsulation(String musicInstrument) {
+        this.musicInstrument = musicInstrument;
+    }
+
+    public String getMusicInstrument() {
+        return musicInstrument;
+    }
+
+    public void setMusicInstrument(String musicInstrument) {
+        this.musicInstrument = musicInstrument;
+    }
 }
